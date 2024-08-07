819face6bb641428ec5b19d2af89fa1833136d4b
MuhammadAbdullah9999 <hafizzabdullah999@gmail.com>
2024-08-07 13:25:33 +0500

    chnages in cors

--- server.js
+++ server.js
@@ -17,10 +17,15 @@ const corsOptions = {
 // Use CORS middleware
 app.use(cors(corsOptions));
 
-// Handle preflight requests
-app.options('*', cors(corsOptions));
-
 // Routes
+app.use(function (req, res, next) {
+  // Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});
+
 app.use('/calculations', calculationsRoutes);
 app.use('/auth', authRoutes);
 app.use('/operations', operationsRoutes);
