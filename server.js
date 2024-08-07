b2cb68c8b301190dc48ce57b5b246f5c8bb219ba
MuhammadAbdullah9999 <hafizzabdullah999@gmail.com>
2024-08-07 05:57:11 +0500

    cors update

--- server.js
+++ server.js
@@ -7,8 +7,19 @@ const operationsRoutes = require('./Routes/Operations');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+const allowedOrigins = ['https://second-assessment-test-frontend.onrender.com'];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin, e.g., mobile apps, curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use('/calculations', calculationsRoutes);
 app.use('/auth', authRoutes);
 app.use('/operations', operationsRoutes);
