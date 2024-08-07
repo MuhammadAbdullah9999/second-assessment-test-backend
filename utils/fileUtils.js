262dc9355eb49a4df5ffca7d8ce49a48eac0d959
MuhammadAbdullah9999 <hafizzabdullah999@gmail.com>
2024-08-07 05:29:09 +0500

    latest

--- utils/fileUtils.js
+++ utils/fileUtils.js
@@ -5,6 +5,7 @@ const dataFilePath = path.join(__dirname, '..', 'data.json');
 const readData = () => {
   try {
     const data = fs.readFileSync(dataFilePath, 'utf8');
+    console.log('Data read successfully:', data);
     return JSON.parse(data);
   } catch (err) {
     console.error('Error reading data:', err);
@@ -15,7 +16,7 @@ const readData = () => {
 const writeData = (data) => {
   try {
     fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), 'utf8');
-    console.log('Data written successfully');
+    console.log('Data written successfully:', data);
   } catch (err) {
     console.error('Error writing data:', err);
   }
